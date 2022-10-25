4fa8d12fbe749c99c40634fde840ef0a9e817a7e
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-10-25 14:21:45 +0200

    add test for create_ensnumber

--- filefinder/tests/test_cmip.py
+++ filefinder/tests/test_cmip.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from filefinder import FileContainer
-from filefinder.cmip import ensure_unique_grid, parse_ens
+from filefinder.cmip import create_ensnumber, ensure_unique_grid, parse_ens
 
 
 def test_parse_ens_cmip5():
@@ -67,6 +67,38 @@ def test_parse_ens_cmip6():
     )
 
     result = parse_ens(fc)
+    assert isinstance(result, FileContainer)
+
+    pd.testing.assert_frame_equal(result.df, expected_df)
+
+
+def test_create_ensnumber():
+    columns = ("filename", "model", "exp", "table", "varn", "ens")
+
+    common = ("exp", "table", "varn")
+
+    records = [
+        ("file0", "CESM2", *common, "r1i1p1f1"),
+        ("file1", "CESM2", *common, "r2i1p1f1"),
+        ("file2", "CESM2", *common, "r3i1p1f1"),
+        ("file3", "UKESM", *common, "r1i2p1f1"),
+        ("file4", "UKESM", *common, "r1i3p1f1"),
+    ]
+
+    df = pd.DataFrame.from_records(
+        records,
+        columns=columns,
+    )
+
+    expected_df = pd.DataFrame.from_records(
+        records,
+        columns=columns,
+    )
+    expected_df["ensnumber"] = (0, 1, 2, 0, 1)
+
+    fc = FileContainer(df)
+
+    result = create_ensnumber(fc)
     assert isinstance(result, FileContainer)
 
     pd.testing.assert_frame_equal(result.df, expected_df)
