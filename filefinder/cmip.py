202cd08b635d3dd9146863be4798a692beee0eaf
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-10-25 10:24:15 +0200

    test and fix cmip filters

--- filefinder/cmip.py
+++ filefinder/cmip.py
@@ -30,7 +30,6 @@ def parse_ens(filelist):
     for key in df.columns:
         filelist.df[key] = df[key].values
 
-    # filelist.df = filelist.df.assign(**df)
     return filelist
 
 
@@ -58,6 +57,6 @@ def ensure_unique_grid(filelist):
     """ensure there is only one grid per simulation"""
 
     # each simulation must be unique in the combination of these keys
-    keys = ["exp", "table", "varn", "model", "ens"]
+    keys = ("exp", "table", "varn", "model", "ens")
 
-    return priority_filter(filelist, VALID_GRIDS, key="grid", groupby=keys)
+    return priority_filter(filelist, column="grid", order=VALID_GRIDS, groupby=keys)
