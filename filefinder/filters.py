8889c28a892838442e112bf9c3ed7001016fb813
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-02-20 14:40:53 +0100

    fix format error

--- filefinder/filters.py
+++ filefinder/filters.py
@@ -124,7 +124,7 @@ def _prioritize(df, key, order, on_missing, multiindex):
             elif on_missing == "skip":
                 pass
             else:
-                raise ValueError(f"on_missing must be one of 'error', 'warn', 'skip'")
+                raise ValueError("on_missing must be one of 'error', 'warn', 'skip'")
 
     df = pd.concat(out, axis=0)
     df = df.reset_index(drop=True)
