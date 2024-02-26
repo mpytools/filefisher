a87bbecd3533ada88825498c47ed4a0460fd7684
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-02-26 18:45:26 +0100

    reinsert import metadata

--- filefinder/__init__.py
+++ filefinder/__init__.py
@@ -2,6 +2,7 @@
 
 from . import _filefinder, cmip, utils
 from ._filefinder import FileContainer, FileFinder
+from importlib.metadata import version as _get_version
 
 try:
     __version__ = _get_version("filefinder")
