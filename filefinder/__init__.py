89049367a748809bead7b43a88b20e85b16451dd
veni-vidi-vici-dormivi <victoria.bauer@env.ethz.ch>
2024-02-26 18:44:42 +0100

    remove version check for import.metadata

--- filefinder/__init__.py
+++ filefinder/__init__.py
@@ -4,14 +4,6 @@ from . import _filefinder, cmip, utils
 from ._filefinder import FileContainer, FileFinder
 
 try:
-    from importlib.metadata import version as _get_version
-except ImportError:
-    # importlib.metadata not available in python 3.7
-    import pkg_resources
-
-    _get_version = lambda pkg: pkg_resources.get_distribution(pkg).version
-
-try:
     __version__ = _get_version("filefinder")
 except Exception:
     # Local copy or not installed with setuptools.
