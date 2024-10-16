593d003420a8eafba56a172ce1cc9dc44ccbd458
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-10-16 23:08:17 +0200

    return empty `FileContainer`

--- filefinder/tests/__init__.py
+++ filefinder/tests/__init__.py
@@ -1,6 +1,10 @@
 import warnings
 from contextlib import contextmanager
 
+import pandas as pd
+
+from filefinder import FileContainer
+
 
 @contextmanager
 def assert_no_warnings():
@@ -8,3 +12,15 @@ def assert_no_warnings():
     with warnings.catch_warnings(record=True) as record:
         yield record
         assert len(record) == 0, "got unexpected warning(s)"
+
+
+def assert_empty_filecontainer(fc, columns=None):
+
+    assert isinstance(fc, FileContainer)
+
+    assert isinstance(fc.df, pd.DataFrame)
+    assert fc.df.index.name == "path"
+    assert len(fc) == 0, f"FileContainer not empty ({len(fc)=})"
+
+    if columns:
+        assert set(fc.df.columns) == set(columns)
