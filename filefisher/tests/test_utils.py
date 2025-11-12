4fe7687383bced9dab451256ee2e89b6bd672346
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-11-12 15:20:47 +0100

    fix tests

--- filefisher/tests/test_utils.py
+++ filefisher/tests/test_utils.py
@@ -3,7 +3,6 @@ import pytest
 from filefisher._utils import (
     _find_keys,
     atoi,
-    find_stack_level,
     natural_keys,
     product_dict,
     update_dict_with_kwargs,
@@ -84,13 +83,3 @@ def test_update_dict_with_kwargs():
 
     with pytest.raises(TypeError, match="missing 1 required positional argument"):
         update_dict_with_kwargs(dictionary={})
-
-
-def test_find_stack_level():
-    assert find_stack_level() == 1
-    assert find_stack_level(test_mode=True) == 2
-
-    def f():
-        return find_stack_level(test_mode=True)
-
-    assert f() == 3
