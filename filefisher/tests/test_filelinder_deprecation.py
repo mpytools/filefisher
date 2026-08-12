8348cdd0b95c72c3b77d35611cf061115b66a946
Mathias Hauser <mathias.hauser@env.ethz.ch>
2026-08-12 15:07:51 +0200

    filefinder: raise ImportError

--- filefisher/tests/test_filelinder_deprecation.py
+++ filefisher/tests/test_filelinder_deprecation.py
@@ -1,25 +1,15 @@
-import sys
 
 import pytest
-
-from . import assert_no_warnings
 
 
 def test_filefinder_deprecation() -> None:
 
-    with pytest.warns(
-        FutureWarning, match="`filefinder` has been renamed to `filefisher`"
+    with pytest.raises(
+        ImportError, match="`filefinder` has been renamed to `filefisher`"
     ):
         import filefinder  # noqa: F401
 
-    # NOTE warning only issued once (even for from imports)
-    with assert_no_warnings():
-        from filefinder import FileContainer  # noqa: F401
-
-    # remove imported module to re-trigger warning
-    del sys.modules["filefinder"]
-
-    with pytest.warns(
-        FutureWarning, match="`filefinder` has been renamed to `filefisher`"
+    with pytest.raises(
+        ImportError, match="`filefinder` has been renamed to `filefisher`"
     ):
-        from filefinder import FileFinder  # noqa: F401 # noqa: F401
+        from filefinder import FileFinder  # noqa: F401
